b7bf9171b6b9460ebe1ebb111edf62da5fc7c6d5
JB Tellez <jb.tellez@gmail.com>
2023-12-05 21:13:08 -0800

    removes stale asset reference

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const weather = require('./assets/weather.json');
 const axios = require('axios');
 
 const app = express();
